c735ee060c304ebf34fd92e4eae670087c8f10dd
Ronson Lui <rlui001@hammer.cs.ucr.edu>
2016-02-12 03:01:47 -0800

    Updated exit shell script

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+# Thi shell script tests the exit command, combined with multiple arguments.
 
+echo "Testing exit with/without arguments:(Press enter to continue): "
+read answer
+./a.out << EOF
+echo "Testing 'exit' by itself."
+exit
+EOF
+echo "============================================================="
+
+echo "Press enter to continue..."
+read answer
+./a.out << EOF
+echo "Testing 'exit' with 'exit' as follow-up argument."
+exit exit
+EOF
+echo "============================================================="
