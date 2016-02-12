340bb55f45df22aedde8b5e1414a9996b82d4d2a
Michael Tran <mtran049@hammer.cs.ucr.edu>
2016-02-11 19:07:37 -0800

    added main.cpp/ included all needed libraries

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Rshell initialized...\n";
+	string userInput;
+	
+	
+	
+
+
+
+
+	return 0;
+}
