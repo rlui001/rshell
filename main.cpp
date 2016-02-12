0ecdc25ab5a30d3f073dce7723a6aea515aab468
Ronson Lui <rlui001@hammer.cs.ucr.edu>
2016-02-12 01:00:44 -0800

    need to update main.cpp, ignore test.cpp, made tests directory

--- main.cpp
+++ main.cpp
@@ -9,30 +9,46 @@
 #include <sys/wait.h>
 #include <string.h>
 
+
+
 using namespace std;
+
 
 int main()
 {
-	system("clear");
-	cout << "Rshell initialized...\n";
-	string username = getlogin();
-	char hostname[128] = "";
-	gethostname(hostname, sizeof(hostname));
-	
-	cout << "[" << username << "@" << hostname << "]" << "$ ";
 
-	string userinput;
-	getline(cin, userinput);	//Temporary method
+    cout << "Rshell initialized...\n";
+    string username = getlogin();
+    char hostname[128] = "";
+    gethostname(hostname, sizeof(hostname));
+ 
+    cout << "[" << username << "@" << hostname << "]" << "$ ";
+    
+    string userinput;
+    getline(cin, userinput); //Temporary method 
+    
+    while (userinput != "exit") //Temporary method
+    {
+        cout << "[" << username << "@" << hostname << "]" << "$ ";
+        
+        // Parse word by word; userinput stores whole line of input.
+        char* cstr = new char [userinput.size() + 1];
+        strcpy(cstr, userinput.c_str());
+        char* token;
+        token = strtok(cstr, " ");
+        while (token != NULL) {
+            cout << token << endl;
+            token = strtok(NULL, " ");
+        }
 
-	while (userinput != "exit") //Temporary method
-	{
-		cout << "[" << username << "@" << hostname << "]" << "$ ";
-		getline(cin, userinput);
-	}
+
+        // getline takes place at end of all operations.
+        getline(cin, userinput);
+
+    }
+   
 
 
 
-
-
-	return 0;
+    return 0;
 }
