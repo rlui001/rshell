0c5d9e9fab134143cf92f9013ccee895d7902ca9
Michael Tran <mtran049@hammer.cs.ucr.edu>
2016-02-12 02:54:38 -0800

    Implemented parsing of commands using strtok_r. Also a brief fork process to execute command tokens.

--- main.cpp
+++ main.cpp
@@ -13,26 +13,106 @@ using namespace std;
 
 int main()
 {
-	system("clear");
-	cout << "Rshell initialized...\n";
-	string username = getlogin();
-	char hostname[128] = "";
-	gethostname(hostname, sizeof(hostname));
-	
-	cout << "[" << username << "@" << hostname << "]" << "$ ";
+	cout << "Rshell initialized...\n\n";
 
-	string userinput;
-	getline(cin, userinput);	//Temporary method
-
-	while (userinput != "exit") //Temporary method
+	// Grabs username.
+	char* username = getlogin();
+	if (getlogin() == NULL)
 	{
-		cout << "[" << username << "@" << hostname << "]" << "$ ";
-		getline(cin, userinput);
+		perror("getlogin() failed");
 	}
 
+	// Grabs user's hostname/machine.
+	char hostname[128] = "";
+	if (gethostname(hostname, sizeof(hostname)) == -1)
+	{
+		perror("gethostname() failed");
+	}
+
+	while (1)
+	{
+		// Command prompt. '~' to distinguish from SSH bash.
+		cout << "~[" << username << "@" << hostname << "]" << "$ ";
+	
+		string userinput;
+		getline(cin, userinput);
+
+		// Discards any input after a '#' character if found.
+		int commentLoc = userinput.find('#');
+		if (commentLoc != string::npos)
+			userinput = userinput.substr(0, commentLoc - 1);
+		
+		// Record order of operators.
+		//int andLoc = userinput.find("||");
+		//int orLoc = userinput.find(
+		//int semiLoc = userinput.find(';');
+		
+		char delim[] = ";|&";
+		char *token, *subtoken;
+		char *saveptr1, *saveptr2;
+
+		char *str1 = new char[userinput.size() + 1];
+		strcpy(str1, userinput.c_str());
+		token = strtok_r(str1, delim, &saveptr1);
+
+		// Iterates through every seperate command in a cmd line.
+		while (token != NULL)
+		{
+			char space[] = " ";
+			subtoken = strtok_r(token, space, &saveptr2);
+
+			// Splits a command into arguments and places them in a vector.
+			vector<string> args;
+			while (subtoken != NULL)
+			{
+				args.push_back(subtoken);
+				cout << "Subtoken: " << subtoken << endl;
+				subtoken = strtok_r(NULL, space, &saveptr2);
+			}
+
+			char **argv = new char*[args.size() + 1];
+
+			for (unsigned int i = 0; i < args.size(); ++i)
+			{
+				argv[i] = new char[args.at(i).size() + 1];
+				strcpy(argv[i], args.at(i).c_str());
+			}
+			
+
+			// Execution of command line using fork calls.
+			int pid = fork();
+			int status = 0;
+
+			if (pid == -1)
+			{
+				perror("fork failed");
+				exit(1);
+			}
+			else if (pid == 0)
+			{
+				if (execvp(*argv, argv) == -1)
+				{
+					perror("execvp failed");
+					exit(1);
+				}
+
+			}
+			else
+			{
+				if (wait(&status) == -1)
+				{
+					perror("waited too long");
+				}
+
+				token = strtok_r(NULL, delim, &saveptr1);
+			}
+
+		}
 
 
-
+		if (userinput == "exit")	//Temporary
+			break;
+	}
 
 	return 0;
 }
