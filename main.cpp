a523b086f420bf489c8f2a556000b5f07df2f770
Ronson Lui <rlui001@hammer.cs.ucr.edu>
2016-02-12 17:34:54 -0800

    Modifications to subtoken to support connectors.

--- main.cpp
+++ main.cpp
@@ -14,108 +14,153 @@ using namespace std;
 
 int main()
 {
-	cout << "Rshell initialized...\n\n";
+    bool andState = false;
+    bool orState = false;
+    bool semiState = false;
 
-	// Grabs username.
-	char *username = getlogin();
-	if (getlogin() == NULL)
-	{
-		perror("getlogin() failed");
-	}
+    // vectors to store commands and symbols
 
-	// Grabs user's hostname/machine.
-	char hostname[128] = "";
-	if (gethostname(hostname, sizeof(hostname)) == -1)
-	{
-		perror("gethostname() failed");
-	}
+    vector<string> storage;
 
-	while (1)
-	{
-		// Command prompt. '~' to distinguish from SSH bash.
-		cout << "~[" << username << "@" << hostname << "]" << "$ ";
-	
-		string userinput;
-		getline(cin, userinput);
+    cout << "Rshell initialized...\n\n";
 
-		// Discards any input after a '#' character if found.
-		int commentLoc = userinput.find('#');
-		if (commentLoc != string::npos)
-			userinput = userinput.substr(0, commentLoc - 1);
-		
-		// Record order of operators.
-		//int andLoc = userinput.find("||");
-		//int orLoc = userinput.find(
-		//int semiLoc = userinput.find(';');
-		
-		char delim[] = ";|&";
-		char *token, *subtoken;
-		char *saveptr1, *saveptr2;
+    // Grabs username.
+    char *username = getlogin();
+    if (getlogin() == NULL)
+    {
+        perror("getlogin() failed");
+    }
 
-		char *str1 = new char[userinput.size() + 1];
-		strcpy(str1, userinput.c_str());
-		token = strtok_r(str1, delim, &saveptr1);
-		
-		// Iterates through every seperate command in a cmd line.
-		while (token != NULL)
-		{
-			char space[] = " ";
-			subtoken = strtok_r(token, space, &saveptr2);
+    // Grabs user's hostname/machine.
+    char hostname[128] = "";
+    if (gethostname(hostname, sizeof(hostname)) == -1)
+    {
+        perror("gethostname() failed");
+    }
 
-			// Splits a command into arguments and places them in a vector.
-			vector<string> args;
-			while (subtoken != NULL)
-			{
-				args.push_back(subtoken);
-				cout << "Subtoken: " << subtoken << endl;
-				subtoken = strtok_r(NULL, space, &saveptr2);
-			}
+    while (1)
+    {
+        // Command prompt. '~' to distinguish from SSH bash.
+        cout << "~[" << username << "@" << hostname << "]" << "$ ";
+    
+        string userinput;
+        getline(cin, userinput);
 
-			char **argv = new char*[args.size() + 1];
-			for (unsigned int i = 0; i < args.size(); ++i)
-			{
-				argv[i] = new char[args.at(i).size() + 1];
-				strcpy(argv[i], args.at(i).c_str());	
-			}
-			argv[args.size()] = '\0';
-			
+        // Discards any input after a '#' character if found.
+        int commentLoc = userinput.find('#');
+        if (commentLoc != string::npos)
+            userinput = userinput.substr(0, commentLoc - 1);
 
-			// Execution of command line using fork calls.
-			int pid = fork();
-			int status = 0;
-
-			if (pid == -1)
-			{
-				perror("fork failed");
-				exit(1);
-			}
-			else if (pid == 0)
-			{
-				if (execvp(*argv, argv) == -1)
-				{
-					perror("execvp failed");
-					exit(1);
-				}
-
-			}
-			else
-			{
-				if (wait(&status) == -1)
-				{
-					perror("waited too long");
-				}
-
-				token = strtok_r(NULL, delim, &saveptr1);
-			}
-
-		}
+        // Record order of operators.
+        
+        string copy = userinput; // create copy of user input to modify
+        
+        // token to take all relevant connectors into vector
 
 
-		if (userinput == "exit")	//Temporary
-			break;
-	}
 
-	return 0;
+        //int orLoc = userinput.find(
+        //int semiLoc = userinput.find(';');
+        
+        char delim[] = "";
+        char *token, *subtoken;
+        char *saveptr1, *saveptr2;
+
+        char *str1 = new char[userinput.size() + 1];
+        strcpy(str1, userinput.c_str());
+        token = strtok_r(str1, delim, &saveptr1);
+        
+        // Iterates through every seperate command in a cmd line.
+        while (token != NULL)
+        {
+            char space[] = " ";
+            subtoken = strtok_r(token, space, &saveptr2);
+
+            // Splits a command into arguments and places them in a vector.
+            vector<string> args;
+            vector<int> split;
+            while (subtoken != NULL)
+            {
+                if (strcmp(subtoken, "&&") == 0) {
+                    split.push_back(1);
+                    cout << "1" << endl;
+                    subtoken = strtok_r(NULL, space, &saveptr2);
+                }
+                else if (strcmp(subtoken, "||") == 0) {
+                    split.push_back(2);
+                    cout << "2" << endl;
+                    subtoken = strtok_r(NULL, space, &saveptr2);
+                }
+                else if (strcmp(subtoken, ";") == 0) {
+                    split.push_back(3);
+                    cout << "3" << endl;
+                    subtoken = strtok_r(NULL, space, &saveptr2);
+                }
+                else {
+                    split.push_back(0);
+                    args.push_back(subtoken);
+                    cout << "Subtoken: " << "0" <<  subtoken << endl;
+                    subtoken = strtok_r(NULL, space, &saveptr2);
+                }
+            }
+
+            for (int i = 0; i < args.size(); ++i) {
+                cout << args[i] << ' ' << split[i] << endl;
+            }
+
+            char **argv = new char*[args.size() + 1];
+            for (unsigned int i = 0; i < args.size(); ++i)
+            {
+                argv[i] = new char[args.at(i).size() + 1];
+                strcpy(argv[i], args.at(i).c_str());	
+            }
+            argv[args.size()] = '\0';
+
+
+            // Execution of command line using fork calls.
+            int pid = fork();
+            int status = 0;
+    
+
+
+
+            if (pid == -1)
+            {
+                perror("fork failed");
+                exit(1);
+            }
+            else if (pid == 0)
+            {
+
+
+
+
+
+                if (execvp(*argv, argv) == -1)
+                {
+                    perror("execvp failed");
+                    exit(1);
+                }
+
+            }
+            else
+            {
+                if (wait(&status) == -1)
+                {
+                    perror("waited too long");
+                }
+
+            token = strtok_r(NULL, delim, &saveptr1);
+            }
+
+        }
+
+
+        if (userinput == "exit")	//Temporary
+            break;
+    }
+
+    return 0;
 }
 
 
