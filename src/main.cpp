242729f9b9811ae7ef2ef006054a1211ead355fb
Ronson Lui <rlui001@hammer.cs.ucr.edu>
2016-03-06 23:46:36 -0800

    Added precedence

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
@@ -34,58 +36,170 @@ int main()
     while (1)
     {
         // Command prompt. '~' to distinguish from SSH bash.
+    restart:
         cout << "~[" << username << "@" << hostname << "]" << "$ ";
 
     string userinput;
     getline(cin, userinput);
+    int lcounter = 0;
+    int rcounter = 0;
+    //string copyinput = userinput;
 
     // Discards any input after a '#' character if found.
     int commentLoc = userinput.find('#');
     if (commentLoc != -1)
         userinput = userinput.substr(0, commentLoc - 1);
 
+    // check if parentheses exist in the string
+    bool hasPrec = false;
+    for (unsigned int i = 0; i < userinput.size(); ++i) {
+        if (userinput[i] == '(' || userinput[i] == ')') {
+            hasPrec = true;
+        }
+    }
+    if (!hasPrec) { // no precedence in string, proceed normally
+        goto no_prec;
+    }
+    
+    // check if # parentheses matches, otherwise return error.
+
+    for (unsigned int i = 0; i < userinput.size(); ++i) {
+        if (userinput[i] == '(') {
+            ++lcounter;
+        }
+        if (userinput[i] == ')') {
+            ++rcounter;
+        }
+    }
+    if (lcounter != rcounter) {
+        // parentheses don't match, output error and start again
+        cout << "Error: misuse of parentheses." << endl;
+        goto restart;
+    }
+
+    // code for precedence begins here... if both checks are positive (parentheses exist in userinput + # of opening/closing parentheses match)
+    
+
+    
+
+
     // Token loop that sets up vector..
-    char lim[] = " ";
-    char *contoken;
-    char *savetok;
+    no_prec:
+    //char lim[] = " ";
+    //char *contoken;
+    //char *savetok;
     vector<int> split;
     //int counter = 0;
     int concount = 0;
+    int pcounter = 0;
 
-    char *tempstr = new char[userinput.size() + 1];
-    strcpy(tempstr, userinput.c_str());
-    contoken = strtok_r(tempstr, lim, &savetok);
+    //char *tempstr = new char[userinput.size() + 1];
+    //strcpy(tempstr, userinput.c_str());
+    //contoken = strtok_r(tempstr, lim, &savetok);
 
-    while (contoken != NULL)
-    {
-        if (strcmp(contoken, "&&") == 0) 
-        {
-        split.push_back(1);
-        contoken = strtok_r(NULL, lim, &savetok);
-        }
+    //while (contoken != NULL)
+    //{
+    //    if (strcmp(contoken, "&&") == 0) 
+    //    {
+    //        split.push_back(1);
+    //        contoken = strtok_r(NULL, lim, &savetok);
+    //    }
 
-        else if (strcmp(contoken, "||") == 0) 
-        {
-        split.push_back(2);
-        contoken = strtok_r(NULL, lim, &savetok);
+    //    else if (strcmp(contoken, "||") == 0) 
+    //    {
+    //        split.push_back(2);
+    //        contoken = strtok_r(NULL, lim, &savetok);
+    //    }
+    //    else if (strcmp(contoken, ";") == 0) 
+    //    {
+    //        split.push_back(3);
+    //        contoken = strtok_r(NULL, lim, &savetok);
+    //    }
+    //    else if (strcmp(contoken, "(") == 0) 
+    //    {
+    //        split.push_back(4);
+    //        contoken = strtok_r(NULL, lim, &savetok);
+    //    }
+    //    else if (strcmp(contoken, ")") == 0) 
+    //    {
+    //        split.push_back(5);
+    //        contoken = strtok_r(NULL, lim, &savetok);
+     //   }
+      //  else 
+    //    {
+    //        //split.push_back(0);
+    //        contoken = strtok_r(NULL, lim, &savetok);
+    //    }
+   // }
+    
+    // alternative to store into split vector
+    unsigned int stringcounter = 0;
+    char* copy = new char[userinput.size() + 1];
+    strcpy(copy, userinput.c_str());
+    while (stringcounter != userinput.size()) {
+        if (copy[stringcounter] == '&') {
+            ++stringcounter;
+            if (copy[stringcounter] == '&') {
+                split.push_back(1);
+                ++stringcounter;
+            }
         }
-        else if (strcmp(contoken, ";") == 0) 
-        {
-        split.push_back(3);
-        contoken = strtok_r(NULL, lim, &savetok);
+        else if (copy[stringcounter] == '|') {
+            ++stringcounter;
+            if (copy[stringcounter] == '|') {
+                split.push_back(2);
+                ++stringcounter;
+            }
         }
-        else 
-        {
-            //split.push_back(0);
-            contoken = strtok_r(NULL, lim, &savetok);
+        else if (copy[stringcounter] == ';') {
+            ++stringcounter;
+            split.push_back(3);
+        }
+        else if (copy[stringcounter] == '(') { 
+            ++stringcounter;
+            split.push_back(4);
+        }
+        else if (copy[stringcounter] == ')') {
+            ++stringcounter;
+            split.push_back(5);
+        }
+        else {
+            ++stringcounter;
         }
     }
+    
 
-    split.push_back(-1); // -1 indicates end of line
 
-    cout << "SIZE OF SPLIT VECTOR: " << split.size() << endl;
 
-    char delim[] = ";|&";
+    
+
+    split.push_back(-1); // indicates end of line
+    char chars[] = "()";
+
+
+    
+    // Discards all parentheses from the string
+    
+
+
+    for (unsigned int i = 0; i < strlen(chars); ++i) {
+
+        userinput.erase (remove(userinput.begin(), userinput.end(), chars[i]), userinput.end());
+    }
+
+    //organize vector index to first connector
+    while (split.at(concount) == 4) {
+        ++concount;
+        ++pcounter;     // may not be needed
+    }
+    
+    
+
+    
+    //skipthis:
+
+
+    char delim[] = ";|&";    
     char *token, *subtoken;
     char *saveptr1, *saveptr2;
 
@@ -96,7 +210,19 @@ int main()
     // Iterates through every seperate command in a cmd line.
     while (token != NULL)
     {
+
         //bool currState = true;
+
+        // repeat above code to organize vector
+        while (split.at(concount) == 4) {
+            ++concount;
+            ++pcounter;     // may not be needed
+        }
+        
+        while (split.at(concount) == 5) {
+            ++concount;
+            --pcounter;
+        }
         char space[] = " ";
         subtoken = strtok_r(token, space, &saveptr2);
 
@@ -104,22 +230,12 @@ int main()
         vector<string> args;
         while (subtoken != NULL)
         {
-        args.push_back(subtoken);
-        //cout << "Subtoken: " << subtoken << endl;
-        subtoken = strtok_r(NULL, space, &saveptr2);
-        }
-        cout << "ARGS SIZE: " << args.size() << endl;
+        
 
-        cout << "TEST: " << endl;
+            args.push_back(subtoken);
 
-        for (unsigned int i = 0; i < split.size(); ++i) {
-            cout << split.at(i) << ' ';
+            subtoken = strtok_r(NULL, space, &saveptr2);
         }
-        cout << endl;
-        for (unsigned int i = 0; i < args.size(); ++i) {
-            cout << args.at(i) << ' ';
-        }
-        cout << endl;
 
         char **argv = new char*[args.size() + 1];
         for (unsigned int i = 0; i < args.size(); ++i)
@@ -272,12 +388,36 @@ int main()
 
         fail_condition:
             if (split.at(concount) == 1) 
-            {	
-                // skip command and go to next. && not satisfied   
-                // fewfw && ls && ls && ls || ls
-                //       1      1    1     2
-                //                         
-                //output should be: (error) ... contents of ls command once
+            {
+                if (pcounter != 0) {        // pcounter != 0 means there are opening parentheses in the userinput
+                    if (split.at(concount+1) == 4) {            // if the following command has parenthesis
+                        ++concount; // moves to ^concount + 1
+                        while (split.at(concount) != 5) {           // until it reaches ')'
+                            token = strtok_r(NULL, delim, &saveptr1);
+                            ++concount;
+                        }
+                        //++concount;     // move past ')' to next connector
+                        goto label2;
+                    }
+                    else {      // no '(' following, which means it is within a set of parentheses, and will skip until correct connector OR ')'
+                        while (split.at(concount) != 5 && split.at(concount) == 1) {
+                            token = strtok_r(NULL, delim, &saveptr1);
+                            ++concount;
+                        }
+                        --pcounter;
+                        goto label2;
+                    }
+                }
+            
+                if (split.at(concount+1) == 4) {
+                    ++concount;
+                    while (split.at(concount) != 5) {
+                        token = strtok_r(NULL, delim, &saveptr1);
+                        ++concount;
+                    }
+                    goto label2;
+                }
+
                 while (split.at(concount) == 1) {
                     token = strtok_r(NULL, delim, &saveptr1);
                     ++concount;
@@ -290,6 +430,7 @@ int main()
             else if (split.at(concount) == 2) 
             {	
                 // || connector conditions satisfied, goes to next command
+
                 goto label2;
                 exit(1);
             }
@@ -299,20 +440,37 @@ int main()
             goto label2;
             exit(1);
             }
-            else {
-                cout << "ERROR HERE!" << endl;
-            }
-
 
         non_fail_condition:
             if (split.at(concount) == 2)
-            {          
-                // || not satisfied, skips command
-                // ls || ls || ls || ls && ls || ls
-                //    2     2     2     1     2
-                //    ^
-                //    ls, ls
-                
+            {                          
+                if (pcounter != 0) {
+                    if (split.at(concount+1) == 4) {
+                        ++concount;
+                        while (split.at(concount) != 5) {
+                            token = strtok_r(NULL, delim, &saveptr1);
+                            ++concount;
+                        }
+                        goto label2;
+                    }
+                    else {
+                        while (split.at(concount) != 5 && split.at(concount) == 2) {
+                            token = strtok_r(NULL, delim, &saveptr1);
+                            ++concount;
+                        }
+                        --pcounter;
+                        goto label2;
+                    }
+                }
+
+                if (split.at(concount+1) == 4) {
+                    ++concount;
+                    while (split.at(concount) != 5) {
+                        token = strtok_r(NULL, delim, &saveptr1);
+                        ++concount;
+                    }
+                    goto label2;
+                }
 
                 while (split.at(concount) == 2) {
                     token = strtok_r(NULL, delim, &saveptr1);
@@ -334,12 +492,13 @@ int main()
                 goto label2;
             }
             goto label2;
-
+            
 
         label: // skips a command, goes to next
             token = strtok_r(NULL, delim, &saveptr1);
 
         label2:
+
             token = strtok_r(NULL, delim, &saveptr1);
 
             ++concount;
@@ -355,4 +514,10 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
 
